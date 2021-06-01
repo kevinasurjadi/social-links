0d4b1d817e47911a6e5c12ea28b7600bc4b4037c
kevinasurjadi <gt_kevin_alexander@hotmail.com>
2021-06-02 00:06:27 +0700

    static

--- app.js
+++ app.js
@@ -22,73 +22,7 @@ function createLinkButtonElements(links, id) {
   });
 }
 
-function fetchWithTimeout(url, options = {}, time = 3000) {
-  const controller = new AbortController();
-  const config = { ...options, signal: controller.signal };
-
-  setTimeout(() => {
-    controller.abort();
-  }, time);
-
-  return fetch(url, config)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`${response.status}: ${response.statusText}`);
-      }
-      return response;
-    })
-    .catch((error) => {
-      if (error.name === 'AbortError') {
-        throw new Error('Response timed out');
-      }
-      throw new Error(error.message);
-    });
-}
-
-(function() {
-  fetchWithTimeout('https://strapi.kevinasurjadi.com/social-links?type=highlight&_sort=priority:ASC')
-    .then((response) => response.json())
-    .then((links) => {
-      if (links.length > 0) {
-        const highlight = document.querySelector('#highlight');
-        const title = document.createElement('p');
-        title.setAttribute('class', 'caption');
-        title.innerHTML = 'HOT!!!';
-        highlight.appendChild(title);
-        createLinkButtonElements(links, 'highlight');
-      }
-    });
-  fetchWithTimeout('https://strapi.kevinasurjadi.com/social-links?type=social&_sort=priority:ASC')
-    .then((response) => response.json())
-    .then((links) => {
-      if (links.length > 0) {
-        const social = document.querySelector('#social');
-        const title = document.createElement('p');
-        title.setAttribute('class', 'caption');
-        title.innerHTML = 'Get to know me better';
-        social.appendChild(title);
-        createLinkButtonElements(links, 'social');
-      }
-    })
-    .catch(async () => {
-      const defaultData = await readDefaultData();
-      const social = document.querySelector('#social');
-      const title = document.createElement('p');
-      title.setAttribute('class', 'caption');
-      title.innerHTML = 'Get to know me better';
-      social.appendChild(title);
-      createLinkButtonElements(defaultData);
-    });
-  fetchWithTimeout('https://strapi.kevinasurjadi.com/social-links?type=workaround&_sort=priority:ASC')
-    .then((response) => response.json())
-    .then((links) => {
-      if (links.length > 0) {
-        const workaround = document.querySelector('#workaround');
-        const title = document.createElement('p');
-        title.setAttribute('class', 'caption');
-        title.innerHTML = 'My workaround';
-        workaround.appendChild(title);
-        createLinkButtonElements(links, 'workaround');
-      }
-    });
+(async function() {
+  const defaultData = await readDefaultData();
+  createLinkButtonElements(defaultData, 'social');
 })();
